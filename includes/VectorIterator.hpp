58d4461a3cf166b4f27c16a439b2170da92386e1
JongeunKeum <jon9eun@gmail.com>
2021-12-22 20:25:34 +0900

    Feat: implement VectorIterator - canonical form, getter

--- includes/VectorIterator.hpp
+++ includes/VectorIterator.hpp
@@ -2,22 +2,32 @@
 #define VECTORITERATOR_HPP
 
 #include "Iterators.hpp"
+#include "utils.hpp"
 
 namespace ft
 {
 	template <typename T>
 	class VectorIterator : public ft::iterator<ft::random_access_iterator_tag, T>
 	{
-		public :
-			typedef typename ft::iterator<ft::random_access_iterator_tag, T>::value_type		value_type;
-			typedef typename ft::iterator<ft::random_access_iterator_tag, T>::difference_type	difference_type;
-			typedef typename ft::iterator<ft::random_access_iterator_tag, T>::iterator_category	iterator_category;
-			typedef typename ft::iterator<ft::random_access_iterator_tag, T>::pointer			pointer;
-			typedef typename ft::iterator<ft::random_access_iterator_tag, T>::reference			reference;
+	public :
+		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::value_type		value_type;
+		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::difference_type	difference_type;
+		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::iterator_category	iterator_category;
+		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::pointer			pointer;
+		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::reference			reference;
 
-		private :
-			
+		VectorIterator(pointer ptr = ft_nullptr) : _ptr(ptr) {}					// Default constructor
+		VectorIterator(const VectorIterator& copy) : _ptr(copy._ptr) {}	// Copy constructor
+		VectorIterator& operator=(const VectorIterator& copy) {
+			if (this != copy)
+				this->_ptr = copy._ptr;
+			return *this;
+		}	// Assignment operator
+		virtual ~VectorIterator() {}	// Destructor
+		pointer const & getPtr() const { return this->_ptr; }	// getter
 
+	private :
+		pointer _ptr;
 	};
 }	// namespace ft
 
