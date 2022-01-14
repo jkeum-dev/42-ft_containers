0997045e9451d9664b5c38c85279f989e99483a6
JongeunKeum <jon9eun@gmail.com>
2022-01-15 01:33:49 +0900

    Feat: add enable_if struct

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -16,6 +16,29 @@ namespace ft
 	private:
 		void operator&() const;	// Can't take address of nullptr
 	} ft_nullptr = {};
+
+	/**
+	 * @brief enable_if
+	 * At this time, we pass the condition we want to check to part B.
+	 * If B is evaluated as true, the type of enable_if::value becomes T.
+	 * And if B is false, value does not exist in enable_if.
+	 * 
+	 * @tparam B is_bool
+	 * @tparam T type
+	 */
+	template <bool B, typename T = void>
+	struct enable_if {};
+
+	/**
+	 * @brief enable_if
+	 * 
+	 * When B is true, type is defined in the specialized version.
+	 * And in other cases, nothing is defined inside it.
+	 * 
+	 * @tparam T type
+	 */
+	template <typename T>
+	struct enable_if<true, T> { typedef T type; };
 } // namespace ft
 
 #endif
