88de4bd167c5e920867ecff29044b82a181621d0
JongeunKeum <jon9eun@gmail.com>
2022-01-20 14:31:12 +0900

    Feat: add lexicographical_compare function

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -140,8 +140,7 @@ namespace ft
 	 * @return false	Not true.
 	 */
 	template <class InputIterator1, class InputIterator2>
-	bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
-	{
+	bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
 		while (first1 != last1) {
 			if (!(*first1 == *first2))
 				return false;
@@ -149,10 +148,11 @@ namespace ft
 			++first2;
 		}
 		return true;
-	}
+	}	// equality
 
 	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
-	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred) {
+	bool equal(InputIterator1 first1, InputIterator1 last1,
+						InputIterator2 first2, BinaryPredicate pred) {
 		while (first1 != last1) {
 			if (!pred(*first1, *first2))
 				return false;
@@ -160,7 +160,52 @@ namespace ft
 			++first2;
 		}
 		return true;
-	}
+	}	// predicate
+
+	/**
+	 * @brief lexicographical_compare
+	 * Lexicographical less-than comparison.
+	 * 
+	 * @tparam InputIterator1		first sequence
+	 * @tparam InputIterator2		second sequence
+	 * @tparam Compare					binary function
+	 * @param first1	initial position of first sequence
+	 * @param last1		final position of first sequence
+	 * @param first2	initial position of second sequence
+	 * @param last2		final position of second sequence
+	 * @param comp		Accepts two arguments of the types pointed by the iterators,
+	 * 								and returns a value convertible to bool.
+	 * @return true		if the first range compares lexicographically less than the second.
+	 * @return false	Not true(including when all the elements of both ranges are equivalent).
+	 */
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+															InputIterator2 first2, InputIterator2 last2) {
+		while (first1 != last1) {
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}	// default
+
+	template <typename InputIterator1, typename InputIterator2, typename Compare>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+															InputIterator2 first2, InputIterator2 last2,
+															Compare comp) {
+		while (first1 != last1) {
+			if (first2 == last2 || comp(*first2, *first1))
+				return false;
+			else if (comp(*first1, *first2))
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}	// custom
 } // namespace ft
 
 #endif
