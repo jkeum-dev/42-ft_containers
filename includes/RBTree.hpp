43810cac671413b1cead3332c5df9a2e26255c5a
JongeunKeum <jon9eun@gmail.com>
2022-02-10 16:18:55 +0900

    Feat: member types & variables

--- includes/RBTree.hpp
+++ includes/RBTree.hpp
@@ -1,27 +1,39 @@
 #ifndef RBTREE_HPP
 #define RBTREE_HPP
 
+#include <stdexcept>
 #include "MapIterator.hpp"
 
 namespace ft
 {
-	template < typename T, typename Compare = ft::less<Key>, typename Alloc = std::allocator<T> >
+	template < typename T, typename Compare = ft::less<T>, typename Alloc = std::allocator<T> >
 	class RBTree {
 	public :
-		typedef T					value_type;
-		typedef T*				pointer;
-		typedef const T*	const_pointer;
-		typedef T&				reference;
-		typedef const T&	const_reference;
-		typedef Compare		value_comp;
-		typedef Alloc			allocator_type;
-		typedef size_t		size_type;
-		typedef ft::RBTreeNode<T>					node_type;
-		typedef ft::MapIterator<T>				iterator;
-		typedef ft::MapIterator<const T>	const_iterator;
+		/**
+		 * @brief Member types
+		 */
+		typedef T																									value_type;
+		typedef T*																								pointer;
+		typedef const T*																					const_pointer;
+		typedef T&																								reference;
+		typedef const T&																					const_reference;
+		typedef Compare																						value_comp;
+		typedef Alloc																							allocator_type;
+		typedef size_t																						size_type;
+		typedef ft::RBTreeNode<T>																	node_type;
+		typedef ft::MapIterator<T>																iterator;
+		typedef ft::MapIterator<const T>													const_iterator;
 		typedef typename Alloc::template rebind<node_type>::other node_allocator_type;
 
-		
+		/**
+		 * @brief Member variables
+		 */
+	private :
+		node_type*					_root;
+		node_type*					_nil;
+		size_type						_size;
+		value_comp					_comp;
+		node_allocator_type	_node_alloc;
 	};
 } // namespace ft
 
